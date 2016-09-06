a34311e78e75936f6b482a58c11788b535dbf42e
sahiljain112 <sahiljain112@gmail.com>
2016-09-06 22:40:15 +0530

    build test

--- src/jspm-boilerplate.js
+++ src/jspm-boilerplate.js
@@ -5,7 +5,7 @@ export class Hello extends React.Component {
   render() {
     return (<div>
          <h1> Hello there </h1>
-         <h2> Using jspm@beta </h2>
+         <h2> Using jspm@beta! </h2>
       </div>
     );
   }
