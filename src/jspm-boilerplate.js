b722780a9bb75db837bba57788d916fcce1f6944
sahiljain112 <sahiljain112@gmail.com>
2016-09-06 18:20:09 +0530

    First commit

---
+++ src/jspm-boilerplate.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export class Hello extends React.Component {
+  render() {
+    return (<div>
+         <h1> Hello there </h1>
+         <h2> Using jspm@beta </h2>
+      </div>
+    );
+  }
+}
